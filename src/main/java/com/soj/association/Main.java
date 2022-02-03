131d9bff3cf629e69c0bfabc2ba656456197dc1c
test@gmail.com <test@gmail.com>
2022-02-03 10:05:26 +0000

    Institute class finished

--- src/main/java/com/soj/association/Main.java
+++ src/main/java/com/soj/association/Main.java
@@ -12,30 +12,30 @@ import java.util.List;
 public class Main {
     static final Logger logger= LogManager.getLogger(Main.class.getName());
     public static void main(String[] args){
-//        Student s1=new Student(1,"Mia","CSE");
-//        Student  s2=new Student(2,"Priya","CSE");
-//        Student s3=new Student(1,"John","EE");
-//        Student  s4=new Student(2,"Jack","EE");
-//
-//        List<Student> cseStudents=new ArrayList<>();
-//        cseStudents.add(s1);
-//        cseStudents.add(s2);
-//        List<Student> eeStudents=new ArrayList<>();
-//        eeStudents.add(s3);
-//        eeStudents.add(s4);
-//
-//        Department cSE=new Department("CSE",cseStudents);
-//        Department eE=new Department("EE",eeStudents);
-//
-//        List<Department> departmentList=new ArrayList<>();
-//        departmentList.add(cSE);
-//        departmentList.add(eE);
-//
-//        Institute institute=new Institute("HIGH LAND",departmentList);
-//
-//        logger.debug("total students in institute:");
-//        logger.debug(institute.getTotalStudentInInstitute());
-//        logger.debug(institute.getInstituteName());
+        Student s1=new Student(1,"Mia","CSE");
+        Student  s2=new Student(2,"Priya","CSE");
+        Student s3=new Student(1,"John","EE");
+        Student  s4=new Student(2,"Jack","EE");
+
+        List<Student> cseStudents=new ArrayList<>();
+        cseStudents.add(s1);
+        cseStudents.add(s2);
+        List<Student> eeStudents=new ArrayList<>();
+        eeStudents.add(s3);
+        eeStudents.add(s4);
+
+        Department cSE=new Department("CSE",cseStudents);
+        Department eE=new Department("EE",eeStudents);
+
+        List<Department> departmentList=new ArrayList<>();
+        departmentList.add(cSE);
+        departmentList.add(eE);
+
+        Institute institute=new Institute("HIGH LAND",departmentList);
+
+        logger.debug("total students in institute:");
+        logger.debug(institute.getTotalStudentInInstitute());
+        logger.debug(institute.getInstituteName());
 
 
     }
