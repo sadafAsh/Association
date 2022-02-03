e0b82f5c8c57f6f923dfce34338e1065c84bc428
test@gmail.com <test@gmail.com>
2022-02-03 10:02:55 +0000

    Department class finished

--- src/main/java/com/soj/association/aggregation/Department.java
+++ src/main/java/com/soj/association/aggregation/Department.java
@@ -1,29 +1,29 @@
-//package com.soj.association.aggregation;
-//
-//import java.util.List;
-//
-//public class Department {
-//    private String name;
-//    private List<Student> students;
-//
-//    public Department(String name, List<Student> students) {
-//        this.name = name;
-//        this.students = students;
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
-//    public List<Student> getStudents() {
-//        return students;
-//    }
-//
-//    public void setStudents(List<Student> students) {
-//        this.students = students;
-//    }
-//}
+package com.soj.association.aggregation;
+
+import java.util.List;
+
+public class Department {
+    private String name;
+    private List<Student> students;
+
+    public Department(String name, List<Student> students) {
+        this.name = name;
+        this.students = students;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+}
